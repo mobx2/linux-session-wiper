7970f6959183d26482fe10eecac48f03e4c4bb95
mobx2 <he.mo234@yahoo.com>
2026-09-17 00:50:08 +0300

    feat(completions): add bash and zsh tab-completion scripts
    Add shell auto-completion support for wiper commands and options.
    
    Co-authored-by: Abdelrahman Arafa <vrfa5503@gmail.com>

---
+++ completions/wiper.zsh
@@ -0,0 +1,19 @@
+#compdef wiper session-cleanup.sh
+
+_wiper() {
+    local -a options
+    options=(
+        '--help[Display help information]'
+        '--version[Display version]'
+        '--history[Wipe shell history]'
+        '--browsers[Wipe browser caches and data]'
+        '--dev[Wipe developer caches]'
+        '--tmp[Wipe session and /tmp files]'
+        '--nuke[Run full session purge]'
+        '--quiet[Run quietly without interactive prompts]'
+    )
+
+    _arguments -s $options
+}
+
+_wiper "$@"
